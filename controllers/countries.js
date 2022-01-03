4846a50482a8fbabf334248afcea4b5f76c64e77
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-01-03 14:13:19 +0630

    delete collection that are not updated

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,17 +1,22 @@
 const Country = require("../models/Country")
 
-exports.addManyCountries = (req, res) => {
-	const countries = req.body.countries
-	countries.map(country => {
-		Country.findOneAndUpdate({code: country.code}, {code: country.code, name: country.name, status: 'updated'}, { upsert: true, new: true, setDefaultsOnInsert: true }, (error, result) => {
-			if(error) {
-				console.log(error);
-				return res.status(500).json({error})
-			}
+exports.addManyCountries = async (req, res) => {
+	try {
+		const countries = req.body.countries
+		countries.map(country => {
+			Country.findOneAndUpdate({code: country.code}, {code: country.code, name: country.name, status: 'updated'}, { upsert: true, new: true, setDefaultsOnInsert: true }, (error, result) => {
+				if(error) {
+					console.log(error);
+					return res.status(500).json({error})
+				}
+			})
 		})
-	})
-
-	res.status(200).json({
-		message: 'Successfully inserted'
-	})
+		await Country.deleteMany({ status: 'default'})
+		await Country.updateMany({}, { status: 'default'})
+		res.status(200).json({
+			message: 'Successfully inserted'
+		})
+	} catch (error) {
+		return res.status(500).json({error})
+	}
 }
